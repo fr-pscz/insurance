edf0884d93fe01a6390ac3ef134ac376a7fc2201
Francesco Pascuzzi <fr.pscz@gmail.com>
2022-06-06 18:35:38 +0200

    Add files via upload

---
+++ EXPN.m
@@ -0,0 +1,19 @@
+%% PARAMS : Expenses
+
+load('riskfree.mat')
+load('survProb.mat')
+
+M            = 50;       % years of coverage
+RD           = 2/100;    % regular deduction
+COMM         = 1.4/100;  % commission fee
+x            = 60;       % age of policyholder
+fixedFees    = 50*1.1;       % yearly fees ex-inflation
+inflation    = 2/100 + 1/100;% yearly inflation
+sigma        = 0.25;     % fund volatility
+probLapse    = 0.15;     % yearly lapse rate
+benefit_cost = 20;       % commission contingent on benefits
+guarantee    = 110/100;  % guaranteed return on premium (case of death)
+premium      = 100000;   % premium
+F0           = premium;
+riskfree     = riskfree; % riskfree zero rates
+survProb     = survProb; % probabilities of survival
